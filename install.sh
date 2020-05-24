95ad15de9ec71300d6a971e73d7a9f0ba4ebe2f4
LazyHacker <github@lazyhacker.com>
2020-05-23 18:49:35 -0700

    Added an install script. Comment out ssh agent for now.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $HOME
+
+echo "source $HOME/dotfiles/bash_profile" >> $HOME/.bash_profile
+
+echo "source $HOME/dotfiles/bashrc" >> $HOME/.bashrc
+
+ln -s $HOME/dotfiles/conkyrc $HOME/.conkyrc
+
+echo "source $HOME/dotfiles/vimrc" >> $HOME/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+
