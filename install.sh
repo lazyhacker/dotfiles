b330c977e9178033a5a42e0bb541db7e8b2d897d
LazyHacker <github@lazyhacker.com>
2026-05-09 22:22:15 -0700

    checks before doing some actions

--- install.sh
+++ install.sh
@@ -16,11 +16,25 @@ ln -s $CURRENT_DIR/conkyrc $HOME/.conkyrc
 
 # Set up Vim and install the plugins.
 echo "source $CURRENT_DIR/vimrc" >> $HOME/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s $CURRENT_DIR/plugins-vim $HOME/.vim/plugins-vim
-vim -u ~/.vimrc -T dumb -n -i NONE -es \
-    -c "silent! PluginInstall" \
-    -c "qall" || true
+VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
+
+# Check if the directory does NOT exist
+if [ ! -d "$VUNDLE_DIR" ]; then
+    echo "Installing Vundle and Vim plugins..."
+
+    # Clone Vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
+
+    # Link your plugin config (using -sf to 'force' in case of a dead link)
+    ln -sf "$CURRENT_DIR/plugins-vim" "$HOME/.vim/plugins-vim"
+
+    # Run the Vim plugin installation
+    vim -u ~/.vimrc -T dumb -n -i NONE -es \
+        -c "silent! PluginInstall" \
+        -c "qall" || true
+else
+    echo "Vundle already installed. Skipping..."
+fi
 
 # ------ Terminal ----------
 
@@ -35,6 +49,8 @@ ln -s $CURRENT_DIR/takuya-powerline.omp.toml $HOME/.oh-my-posh.omp.toml
 #oh-my-posh config export --config $CURRENT_DIR/takuya-powerline.omp.json --format toml > $HOME/.oh-my-posh.omp.toml
 
 # Configure Alacritty terminal
+mkdir -p $HOME/.config/alacritty
+touch $HOME/.config/alacritty.toml
 echo "import = [\"$CURRENT_DIR/alacritty.toml\"]" | cat - $HOME/.config/alacritty/alacritty.toml > temp.toml && mv temp.toml ~/.config/alacritty/alacritty.toml
 
 # On Linux, et up fallback fonts for symbols in Terminus.
