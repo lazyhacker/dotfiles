c816121d632d5cab358de440217f69836784bf24
LazyHacker <github@lazyhacker.com>
2026-05-09 23:00:54 -0700

    adding safety checks

--- install.sh
+++ install.sh
@@ -3,63 +3,70 @@
 CURRENT_DIR=$(pwd)
 cd $HOME
 
+# Helper function: Append line if it doesn't exist
+append_if_missing() {
+    LINE=$1
+    FILE=$2
+    touch "$FILE"
+    if ! grep -Fxq "$LINE" "$FILE"; then
+        echo "$LINE" >> "$FILE"
+    fi
+}
+
+# Helper function: Create symlink only if it doesn't exist or points elsewhere
+link_if_missing() {
+    SRC=$1
+    DEST=$2
+    # Check if DEST is a symlink and if it already points to SRC
+    if [ -L "$DEST" ] && [ "$(readlink "$DEST")" = "$SRC" ]; then
+        return 0 # Already correctly linked
+    else
+        echo "Linking $DEST -> $SRC"
+        ln -sf "$SRC" "$DEST"
+    fi
+}
+
 # Set up bash configuration
-echo "source $CURRENT_DIR/bash_profile" >> $HOME/.bash_profile
-echo "source $CURRENT_DIR/bashrc" >> $HOME/.bashrc
+append_if_missing "source $CURRENT_DIR/bash_profile" "$HOME/.bash_profile"
+append_if_missing "source $CURRENT_DIR/bashrc" "$HOME/.bashrc"
 
 # Setup TMUX
-echo "source-file $CURRENT_DIR/tmux.conf" >> $HOME/.tmux.conf
-#ln -s $CURRENT_DIR/tmux.conf $HOME/.tmux.conf
+append_if_missing "source-file $CURRENT_DIR/tmux.conf" "$HOME/.tmux.conf"
 
 # Configure Conky
-ln -s $CURRENT_DIR/conkyrc $HOME/.conkyrc
+link_if_missing "$CURRENT_DIR/conkyrc" "$HOME/.conkyrc"
 
-# Set up Vim and install the plugins.
-echo "source $CURRENT_DIR/vimrc" >> $HOME/.vimrc
+# Set up Vim
+append_if_missing "source $CURRENT_DIR/vimrc" "$HOME/.vimrc"
 VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
 
-# Check if the directory does NOT exist
 if [ ! -d "$VUNDLE_DIR" ]; then
-    echo "Installing Vundle and Vim plugins..."
-
-    # Clone Vundle
+    echo "Installing Vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
-
-    # Link your plugin config (using -sf to 'force' in case of a dead link)
-    ln -sf "$CURRENT_DIR/plugins-vim" "$HOME/.vim/plugins-vim"
-
-    # Run the Vim plugin installation
-    vim -u ~/.vimrc -T dumb -n -i NONE -es \
-        -c "silent! PluginInstall" \
-        -c "qall" || true
-else
-    echo "Vundle already installed. Skipping..."
+    link_if_missing "$CURRENT_DIR/plugins-vim" "$HOME/.vim/plugins-vim"
+    vim -u ~/.vimrc -T dumb -n -i NONE -es -c "silent! PluginInstall" -c "qall" || true
 fi
 
 # ------ Terminal ----------
 
-# Configure keybindings for Readline in the terminal
-ln -s $CURRENT_DIR/inputrc $HOME/.inputrc
+link_if_missing "$CURRENT_DIR/inputrc" "$HOME/.inputrc"
+link_if_missing "$CURRENT_DIR/dir_colors" "$HOME/.dircolors"
+link_if_missing "$CURRENT_DIR/takuya-powerline.omp.toml" "$HOME/.oh-my-posh.omp.toml"
 
-# Colorize the file system.
-ln -s $CURRENT_DIR/dir_colors $HOME/.dircolors
-
-# Configure the prompt
-ln -s $CURRENT_DIR/takuya-powerline.omp.toml $HOME/.oh-my-posh.omp.toml
-#oh-my-posh config export --config $CURRENT_DIR/takuya-powerline.omp.json --format toml > $HOME/.oh-my-posh.omp.toml
-
-# Configure Alacritty terminal
+# Alacritty logic
 ALACRITTY_CONFIG_DIR=$HOME/.config/alacritty
 ALACRITTY_CONFIG=$ALACRITTY_CONFIG_DIR/alacritty.toml
-mkdir -p $ALACRITTY_CONFIG_DIR
-touch $ALACRITTY_CONFIG
-printf "[general]\nimport = [\"$CURRENT_DIR/alacritty/alacritty.toml\"]\n\n" | cat - $ALACRITTY_CONFIG > temp.toml && mv temp.toml $ALACRITTY_CONFIG
+mkdir -p "$ALACRITTY_CONFIG_DIR"
+touch "$ALACRITTY_CONFIG"
 
-# On Linux, et up fallback fonts for symbols in Terminus.
-# This assumes that the font being used is Terminus and the fallback is
-# Cascadia Mono nerd font.
-if [[ "$(uname)" == "Linux" ]]; then
-    mkdir -p $HOME/.config/fontconfig/conf.d
-    ln -s $CURRENT_DIR/fontconfig/conf.d/99-terminus-fallback.conf $HOME/.config/fontconfig/conf.d
+IMPORT_PATH="$CURRENT_DIR/alacritty/alacritty.toml"
+if ! grep -q "$IMPORT_PATH" "$ALACRITTY_CONFIG"; then
+    printf "[general]\nimport = [\"%s\"]\n\n" "$IMPORT_PATH" | cat - "$ALACRITTY_CONFIG" > temp.toml && mv temp.toml "$ALACRITTY_CONFIG"
+fi
+
+# Fontconfig (Linux only)
+if [ "$(uname)" = "Linux" ]; then
+    mkdir -p "$HOME/.config/fontconfig/conf.d"
+    link_if_missing "$CURRENT_DIR/fontconfig/conf.d/99-terminus-fallback.conf" "$HOME/.config/fontconfig/conf.d/99-terminus-fallback.conf"
     fc-cache -fv
 fi
